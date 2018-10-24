ddbac21257050c237fb98b1447e124f1b49843d3
Michael Anthony Allan <michael.anthony.allan@gmail.com>
2018-10-24 14:14:55 +0100

    Removes house adds Tb Houses, refactored students for Houses foreign key

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,25 @@
 require_relative('../models/student')
+require_relative('../models/house')
 require('pry-byebug')
 
 
+house1 = House.new({'name' => 'Gryffindor'})
+house2 = House.new({'name' => 'Slytherin'})
+house3 = House.new({'name' => 'Hufflepuff'})
+house4 = House.new({'name' => 'Ravenclaw'})
+
+house1.save()
+house2.save()
+house3.save()
+house4.save()
 
 
-
-student1 = Student.new({'first_name' => 'Harry', 'last_name' => 'Potter', 'age' => 14, 'house' => 'Gryffindor'})
-student2 = Student.new({'first_name' => 'Ron', 'last_name' => 'Weasley', 'age' => 14, 'house' => 'Gryffindor'})
-student3 = Student.new({'first_name' => 'Hermione', 'last_name' => 'Granger', 'age' => 14, 'house' => 'Gryffindor'})
-student4 = Student.new({'first_name' => 'Draco', 'last_name' => 'Malfoy', 'age' => 14, 'house' => 'Slytherin'})
-student5 = Student.new({'first_name' => 'Luna', 'last_name' => 'Lovegood', 'age' => 14, 'house' => 'Ravenclaw'})
-student6 = Student.new({'first_name' => 'Cedric', 'last_name' => 'Diggory', 'age' => 17, 'house' => 'Hufflepuff'})
+student1 = Student.new({'first_name' => 'Harry', 'last_name' => 'Potter', 'age' => 14, 'house_id' => house1.id})
+student2 = Student.new({'first_name' => 'Ron', 'last_name' => 'Weasley', 'age' => 14, 'house_id' => house1.id})
+student3 = Student.new({'first_name' => 'Hermione', 'last_name' => 'Granger', 'age' => 14, 'house_id' => house1.id})
+student4 = Student.new({'first_name' => 'Draco', 'last_name' => 'Malfoy', 'age' => 14, 'house_id' => house2.id})
+student5 = Student.new({'first_name' => 'Luna', 'last_name' => 'Lovegood', 'age' => 14, 'house_id' => house3.id})
+student6 = Student.new({'first_name' => 'Cedric', 'last_name' => 'Diggory', 'age' => 17, 'house_id' => house4.id})
 
 student1.save()
 student2.save()
@@ -18,3 +27,6 @@ student3.save()
 student4.save()
 student5.save()
 student6.save()
+
+binding.pry
+nil
--- models/student.rb
+++ models/student.rb
@@ -3,21 +3,21 @@ require_relative("../db/sql_runner.rb")
 class Student
 
   attr_reader :id
-  attr_accessor :first_name, :last_name, :age, :house
+  attr_accessor :first_name, :last_name, :age, :house_id
 
   def initialize(options)
     @id = options['id'].to_i if options['id']
     @first_name = options['first_name']
     @last_name = options['last_name']
     @age = options['age'].to_i
-    @house = options['house']
+    @house_id = options['house_id']
   end
 
   def save()
-    sql = "INSERT INTO students (first_name, last_name, age, house)
+    sql = "INSERT INTO students (first_name, last_name, age, house_id)
       VALUES ($1, $2, $3, $4)
       RETURNING id"
-    values = [@first_name, @last_name, @age, @house]
+    values = [@first_name, @last_name, @age, @house_id]
     student_data = SqlRunner.run(sql, values)
     @id = student_data.first()['id'].to_i
   end
